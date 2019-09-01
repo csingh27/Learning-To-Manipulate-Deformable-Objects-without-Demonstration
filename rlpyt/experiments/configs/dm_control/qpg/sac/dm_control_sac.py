b0cda2e2f48625d7d97146a902ea84c0184f9c19
Wilson Yan <wilson1.yan@berkeley.edu>
2019-08-31 23:42:33 -0700

    Added SAC V2 experiments

--- rlpyt/experiments/configs/dm_control/qpg/sac/dm_control_sac.py
+++ rlpyt/experiments/configs/dm_control/qpg/sac/dm_control_sac.py
@@ -48,3 +48,49 @@ config = dict(
 )
 
 configs["sac_1M_serial"] = config
+
+config = dict(
+    agent=dict(
+        q_model_kwargs=None,
+        model_kwargs=None,
+    ),
+    algo=dict(
+        discount=0.99,
+        batch_size=256,
+        target_update_tau=0.005,
+        target_update_interval=1,
+        learning_rate=3e-4,
+        reparameterize=True,
+        policy_output_regularization=0.0,
+        reward_scale=1
+    ),
+    model=dict(),
+    optim=dict(),
+    runner=dict(
+        n_steps=1e6,
+        log_interval_steps=1e4,
+    ),
+    sampler=dict(
+        batch_T=1,
+        batch_B=1,
+        max_decorrelation_steps=0,
+        eval_n_envs=10,
+        eval_max_steps=20000,
+        eval_max_trajectories=50,
+    ),
+    env=dict(
+        domain='cloth_v0',
+        task='easy',
+        max_path_length=1200,
+        task_kwargs=dict(reward='diagonal')
+    ),
+    eval_env=dict(
+        domain='cloth_v0',
+        task='easy',
+        max_path_length=1200,
+        task_kwargs=dict(reward='diagonal')
+    )
+)
+
+configs["sac_1M_serial_v2"] = config
+
