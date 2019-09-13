cb8ad2947c20145267557dee08bc65a9f5de6184
Wilson Yan <wilson1.yan@berkeley.edu>
2019-09-13 03:04:51 -0700

    basically tehre

--- rlpyt/experiments/scripts/dm_control/qpg/sac/launch/launch_dm_control_sac_pixels_cloth_point.py
+++ rlpyt/experiments/scripts/dm_control/qpg/sac/launch/launch_dm_control_sac_pixels_cloth_point.py
@@ -5,31 +5,32 @@ from rlpyt.utils.launching.variant import make_variants, VariantLevel
 
 script = "rlpyt/experiments/scripts/dm_control/qpg/sac/train/dm_control_sac.py"
 affinity_code = encode_affinity(
-    n_cpu_core=20,
+    n_cpu_core=24,
     n_gpu=4,
     contexts_per_gpu=1,
 )
 
 runs_per_setting = 4
-default_config_key = "sac_state_cloth_corner"
-experiment_title = "sac_dm_control_state_cloth_point"
+default_config_key = "sac_pixels_cloth_corner"
+experiment_title = "sac_dm_control_pixels_cloth_point"
 variant_levels = list()
 
-domain = ['cloth_point_state']
+domain = ['cloth_point']
 task = ['easy']
 values = list(zip(domain, task))
 dir_names = ["env_{}_{}".format(*v) for v in values]
 keys = [('env', 'domain'), ('env', 'task')]
 variant_levels.append(VariantLevel(keys, values, dir_names))
 
-model_cls = ['PiMlpModel']
-random_location = [True]
-sac_module = ['sac_v2']
-sac_agent_module = ['sac_agent_v2']
-values = list(zip(model_cls, random_location, sac_module, sac_agent_module))
+model_cls = ['PiConvModel']#, 'GumbelPiConvModel']
+random_location = [True]#, False]
+sac_module = ['sac_v2']#, 'sac_v2_generic']
+sac_agent_module = ['sac_agent_v2']#, 'sac_agent_v2_generic']
+state_keys= [['location', 'pixels']]#, ['pixels']]
+values = list(zip(model_cls, random_location, sac_module, sac_agent_module, state_keys))
 dir_names = ["model_cls_{}_rnd_loc_{}".format(*v) for v in values]
 keys = [('agent', 'ModelCls'), ('env', 'task_kwargs', 'random_location'),
-        ('sac_module',), ('sac_agent_module',)]
+        ('sac_module',), ('sac_agent_module',), ('state_keys',)]
 variant_levels.append(VariantLevel(keys, values, dir_names))
 
 variants, log_dirs = make_variants(*variant_levels)
