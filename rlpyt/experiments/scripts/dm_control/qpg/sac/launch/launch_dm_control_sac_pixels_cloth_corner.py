a4f8ef1191fc83641742271b897a918e2d4dfed9
Wilson Yan <wilson1.yan@berkeley.edu>
2019-09-13 19:48:26 -0700

    exp

--- rlpyt/experiments/scripts/dm_control/qpg/sac/launch/launch_dm_control_sac_pixels_cloth_corner.py
+++ rlpyt/experiments/scripts/dm_control/qpg/sac/launch/launch_dm_control_sac_pixels_cloth_corner.py
@@ -6,7 +6,7 @@ from rlpyt.utils.launching.variant import make_variants, VariantLevel
 script = "rlpyt/experiments/scripts/dm_control/qpg/sac/train/dm_control_sac.py"
 affinity_code = encode_affinity(
     n_cpu_core=24,
-    n_gpu=8,
+    n_gpu=4,
     contexts_per_gpu=1,
 )
 
@@ -22,11 +22,11 @@ dir_names = ["env_{}_{}".format(*v) for v in values]
 keys = [('env', 'domain'), ('env', 'task')]
 variant_levels.append(VariantLevel(keys, values, dir_names))
 
-model_cls = ['PiConvModel', 'GumbelPiConvModel']
-random_location = [True, False]
-sac_module = ['sac_v2', 'sac_v2_generic']
-sac_agent_module = ['sac_agent_v2', 'sac_agent_v2_generic']
-state_keys= [['location', 'pixels'], ['pixels']]
+model_cls = ['PiConvModel']#, 'GumbelPiConvModel']
+random_location = [True]#, False]
+sac_module = ['sac_v2']#, 'sac_v2_generic']
+sac_agent_module = ['sac_agent_v2']#, 'sac_agent_v2_generic']
+state_keys= [['location', 'pixels']]#, ['pixels']]
 values = list(zip(model_cls, random_location, sac_module, sac_agent_module, state_keys))
 dir_names = ["model_cls_{}_rnd_loc_{}".format(*v) for v in values]
 keys = [('agent', 'ModelCls'), ('env', 'task_kwargs', 'random_location'),
